874d37a4f2ddbb727a22380e12dcf04ae535ab40
KairigoShawn <kairigonjogu@outlook.com>
2021-10-13 06:21:28 +0300

    imports the newquote component into the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewquoteComponent } from '../newquote/newquote.component';
+import { NewquoteComponent } from './newquote/newquote.component';
 
 @NgModule({
   declarations: [
